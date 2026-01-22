b9053e525b37b51502046b8a1da9c14ee9d3b397
Jonas Tingeborn <134889+jojje@users.noreply.github.com>
2026-01-22 13:24:33 +0100

    Refactor extract table specific rendering
    Prepare for supporting multiple output formats

--- cmd/pod/getPod.go
+++ cmd/pod/getPod.go
@@ -23,55 +23,59 @@ var GetPodCmd = &cobra.Command{
 		pods, err := api.GetPods()
 		cobra.CheckErr(err)
 
-		data := make([][]string, len(pods))
-		for i, p := range pods {
-			if len(args) == 1 && p.Id != strings.ToLower(args[0]) {
-				continue
-			}
-			row := []string{p.Id, p.Name, fmt.Sprintf("%d %s", p.GpuCount, p.Machine.GpuDisplayName), p.ImageName, p.DesiredStatus}
-			if AllFields {
-
-				var portEntries int = 0
-				if p.Runtime != nil && p.Runtime.Ports != nil {
-					portEntries = len(p.Runtime.Ports)
-				}
-				ports := make([]string, portEntries)
-				for j := range ports {
-					var privpub string = "prv"
-					if p.Runtime.Ports[j].IsIpPublic {
-						privpub = "pub"
-					}
-					ports[j] = fmt.Sprintf("%s:%d->%d\u00A0(%s,%s)", p.Runtime.Ports[j].Ip, p.Runtime.Ports[j].PublicPort, p.Runtime.Ports[j].PrivatePort, privpub, p.Runtime.Ports[j].PortType)
-				}
-
-				row = append(
-					row,
-					p.PodType,
-					fmt.Sprintf("%d", p.VcpuCount),
-					fmt.Sprintf("%d", p.MemoryInGb),
-					fmt.Sprintf("%d", p.ContainerDiskInGb),
-					fmt.Sprintf("%d", p.VolumeInGb),
-					fmt.Sprintf("%s", p.Machine.Location),
-					fmt.Sprintf("%.3f", p.CostPerHr),
-					fmt.Sprintf("%s", strings.Join(ports[:], ",")),
-				)
-			}
-			data[i] = row
-		}
-
-		header := []string{"ID", "Name", "GPU", "Image Name", "Status"}
-		if AllFields {
-			header = append(header, "Pod Type", "vCPU", "Mem", "Container Disk", "Volume Disk", "Location", "$/hr", "Ports")
-		}
-
-		tb := tablewriter.NewWriter(os.Stdout)
-		tb.SetHeader(header)
-		tb.AppendBulk(data)
-		format.TableDefaults(tb)
-		tb.Render()
+		toTable(pods, args)
 	},
 }
 
 func init() {
 	GetPodCmd.Flags().BoolVarP(&AllFields, "allfields", "a", false, "include all fields in output")
+}
+
+func toTable(pods []*api.Pod, args []string) {
+	data := make([][]string, len(pods))
+	for i, p := range pods {
+		if len(args) == 1 && p.Id != strings.ToLower(args[0]) {
+			continue
+		}
+		row := []string{p.Id, p.Name, fmt.Sprintf("%d %s", p.GpuCount, p.Machine.GpuDisplayName), p.ImageName, p.DesiredStatus}
+		if AllFields {
+
+			var portEntries int = 0
+			if p.Runtime != nil && p.Runtime.Ports != nil {
+				portEntries = len(p.Runtime.Ports)
+			}
+			ports := make([]string, portEntries)
+			for j := range ports {
+				var privpub string = "prv"
+				if p.Runtime.Ports[j].IsIpPublic {
+					privpub = "pub"
+				}
+				ports[j] = fmt.Sprintf("%s:%d->%d\u00A0(%s,%s)", p.Runtime.Ports[j].Ip, p.Runtime.Ports[j].PublicPort, p.Runtime.Ports[j].PrivatePort, privpub, p.Runtime.Ports[j].PortType)
+			}
+
+			row = append(
+				row,
+				p.PodType,
+				fmt.Sprintf("%d", p.VcpuCount),
+				fmt.Sprintf("%d", p.MemoryInGb),
+				fmt.Sprintf("%d", p.ContainerDiskInGb),
+				fmt.Sprintf("%d", p.VolumeInGb),
+				fmt.Sprintf("%s", p.Machine.Location),
+				fmt.Sprintf("%.3f", p.CostPerHr),
+				fmt.Sprintf("%s", strings.Join(ports[:], ",")),
+			)
+		}
+		data[i] = row
+	}
+
+	header := []string{"ID", "Name", "GPU", "Image Name", "Status"}
+	if AllFields {
+		header = append(header, "Pod Type", "vCPU", "Mem", "Container Disk", "Volume Disk", "Location", "$/hr", "Ports")
+	}
+
+	tb := tablewriter.NewWriter(os.Stdout)
+	tb.SetHeader(header)
+	tb.AppendBulk(data)
+	format.TableDefaults(tb)
+	tb.Render()
 }
